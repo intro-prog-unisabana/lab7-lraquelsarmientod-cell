515a7444ff801f69ecbe46ce324264203d40866c
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-04-09 10:53:16 -0500

    5

--- password_manager.py
+++ password_manager.py
@@ -30,10 +30,29 @@ def encrypt_passwords_in_file(filename: str) -> None:
         writer.writerows(rows)
 
 
-
 def change_password(filename: str, website: str, password: str) -> bool:
     """TODO: Parte 3."""
-    pass
+    
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        rows = [row for row in reader if row]
+    
+    found = False
+    
+    for i in range(1, len(rows)):
+        if rows[i][0] == website:
+            rows[i][2] = caesar_encrypt(password)
+            found = True
+            break
+    
+    if not found:
+        return False
+    
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+    
+    return True
 
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
